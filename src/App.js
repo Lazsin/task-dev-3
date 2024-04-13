e5485e6c81eea6d15b5b82e81d1d853c36815a20
Ivan Demchenko <demchenko.vanya2003@gmail.com>
2024-04-13 19:31:00 +0300

    f

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [selectedText, setSelectedText] = useState('');
+  const [highlightedText, setHighlightedText] = useState('');
+  const [cursorPosition, setCursorPosition] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setSelectedText(inputText);
+  };
+
+  const handleMouseDown = (e) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseUp = () => {
+    setCursorPosition(null);
+  };
+
+  const handleMouseMove = (e) => {
+    if (cursorPosition) {
+      const dx = e.clientX - cursorPosition.x;
+      const dy = e.clientY - cursorPosition.y;
+    }
+  };
+
+  const handleTextSelection = (e) => {
+    const selection = window.getSelection().toString();
+    setHighlightedText(selection);
+  };
+
   return (
     <div className="App">
-      <form className='form'>
-        <input placeholder='1234'></input>
-        <input placeholder='abcd'></input>
-        <button>button</button>
-      </form>
-      
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button onClick={handleButtonClick}>Apply</button>
+      <div>{selectedText}</div>
+      <div
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseUpCapture={handleTextSelection}
+      >
+        {highlightedText ? (
+          <span style={{ backgroundColor: 'yellow' }}>{highlightedText}</span>
+        ) : (
+          <span>{selectedText}</span>
+        )}
+      </div>
     </div>
   );
 }
